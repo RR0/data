bed8305e30afc767353a6e9d3b40f02a08ec4a0a
javarome <javarome@gmail.com>
2025-02-22 23:41:27 +0100

    Fix event type parsing

---
+++ src/event/EventDataFactory.test.ts
@@ -0,0 +1,41 @@
+import { describe, expect, test } from "@javarome/testscript"
+import { EventDataFactory } from "./EventDataFactory"
+import { RR0EventFactory } from "./RR0EventFactory"
+import { FileContents, FileContentsLang } from "@javarome/fileutil"
+
+describe("EventDatafactory", () => {
+
+  test("build people with no name", () => {
+    const eventFactory = new RR0EventFactory()
+    const factory = new EventDataFactory(eventFactory, "sighting", ["index.json"])
+    const lang = new FileContentsLang()
+    lang.lang = ""
+    lang.variants = []
+    const file = new FileContents("test/time/2/0/2/0/12/21/ConjonctionSaturneJupiter/index.json", "utf-8", `{
+  "type": "event",
+  "eventType": "sighting",
+  "time": "2020-12-21 ~06:00",
+  "place": {
+    "name": "Spicheren (Moselle)"
+  },
+  "description": "conjonction de Jupiter et Saturne dans le ciel, pris en photo par le Dr Sebastian Voltmer astro-photographe allemand résidant en Moselle-Est",
+  "image": "saturne-jupiter.jpg",
+  "sources": [
+    {
+      "authors": [
+        "Stéphane MAZZUCOTELLI"
+      ],
+      "url": "https://www.republicain-lorrain.fr/culture-loisirs/2020/12/21/planetes-le-rapprochement-de-jupiter-et-saturne-dans-le-ciel-de-spicheren-pres-de-forbach",
+      "title": "Planètes : le rapprochement de Jupiter et Saturne dans le ciel de Spicheren, près de Forbach",
+      "publication": {
+        "publisher": "Le Républicain Lorrain",
+        "time": "2020-12-21"
+      }
+    }
+  ]
+}
+`, new Date("2025-02-22T22:22:07.720Z"), lang)
+    const event = factory.create(file)
+    expect(event.eventType).toEqual("sighting")
+  })
+})
--- src/event/EventDataFactory.ts
+++ src/event/EventDataFactory.ts
@@ -1,8 +1,7 @@
-import { RR0Event, RR0EventFactory } from "./index"
+import { RR0Event, RR0EventFactory, RR0EventJson } from "./index"
 import { FileContents } from "@javarome/fileutil"
-import { TypedDataFactory } from "../TypedDataFactory"
-import { RR0EventJson } from "./RR0EventJson"
-import { RR0EventType } from "./RR0EventType"
+import { TypedDataFactory } from "../TypedDataFactory.js"
+import { RR0EventType } from "./RR0EventType.js"
 
 /**
  * A RR0Data factory which can read either <someType>.json files of index.json with a "type": "<someType>" property.
@@ -14,10 +13,17 @@ export class EventDataFactory<T extends RR0EventType = RR0EventType> extends Typ
     super(eventFactory, "event", fileNames)
   }
 
+
+  parse(dataJson: RR0EventJson): RR0Event {
+    const data = super.parse(dataJson)
+    data.eventType = dataJson.eventType
+    return data
+  }
+
   create(file: FileContents): RR0Event<T> | undefined {
     const event = super.create(file) as RR0Event<T>
     let eventData: RR0Event<T> | undefined
-    if (event?.eventType === this.eventType) {
+    if (this.eventType === event?.eventType) {
       eventData = event
     }
     return eventData
