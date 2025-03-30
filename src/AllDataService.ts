de1b2157ac814e4f9752806b590dcb12a85b2cef
javarome <javarome@gmail.com>
2025-03-30 19:53:47 +0200

    split real one and all data files

--- src/AllDataService.ts
+++ src/AllDataService.ts
@@ -26,30 +26,35 @@ export class AllDataService {
     const key = dirName + "$" + fileNames.join("$")
     let dataList = this.pathToData.get(key)
     if (dataList === undefined) {
-      dataList = await this.read<T>(dirName, fileNames)
+      dataList = await this.readAll<T>(dirName, fileNames)
       this.pathToData.set(key, dataList)
     }
     return dataList.filter(data => types.includes(data.type)) as T[]
   }
 
-  protected async read<T extends RR0Data = RR0Data>(dirName: string, fileNames: string[]): Promise<T[]> {
+  protected async read<T extends RR0Data = RR0Data>(fileName: string): Promise<T> {
+    const dataFile = FileContents.read(fileName, "utf-8")
+    let data: T | undefined
+    for (let i = 0; !data && i < this.factories.length; i++) {
+      const factory = this.factories[i]
+      try {
+        data = (factory.createFromFile(dataFile) as T)
+      } catch (e) {
+        console.warn("Could not create a", factory.type, "from", dataFile, "because of", e)
+      }
+    }
+    return data
+  }
+
+  protected async readAll<T extends RR0Data = RR0Data>(dirName: string, fileNames: string[]): Promise<T[]> {
     const dataList: T[] = []
-    const p = dirName + "/*(" + fileNames.join("|") + ")"
+    const p = `${dirName}/*(${fileNames.join("|")})`
     const files = globSync(p)
     for (const file of files) {
       try {
-        const dataFile = FileContents.read(file, "utf-8")
-        let data: RR0Data
-        for (let i = 0; !data && i < this.factories.length; i++) {
-          const factory = this.factories[i]
-          try {
-            data = factory.createFromFile(dataFile)
-          } catch (e) {
-            console.warn("Could not create a", factory.type, "from", dataFile, "because of", e)
-          }
-        }
-        if (data) {
-          dataList.push(data as T)
+        const dataFile = await this.read(file)
+        if (dataFile) {
+          dataList.push(dataFile as T)
         } else {
           throw new Error("No factory to handle " + dataFile.name)
         }
